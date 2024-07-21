4f4bf9024e43fea36173deed79e6dd2b2f68b838
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-21 09:43:52 +0530

    add listapp master

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const addButton = document.getElementById('add-button');
     const addInput = document.getElementById('add-input');
     const listContainer = document.getElementById('list');
+    const template = document.getElementById('list-item-template').content;
 
     addButton.addEventListener('click', () => {
         const itemText = addInput.value.trim();
         if (itemText !== '') {
-            const listItem = document.createElement('li');
-            listItem.textContent = itemText;
-            listContainer.appendChild(listItem);
+            const newItem = document.importNode(template, true);
+            newItem.querySelector('span').textContent = itemText;
+            listContainer.appendChild(newItem);
             addInput.value = '';
         }
     });
